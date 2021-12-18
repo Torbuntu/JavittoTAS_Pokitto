6ed7a64f95da42c3d912c061cb203679bb8d9d76
torbuntu <torbuntu@fedoraproject.org>
2021-12-18 16:04:00 -0600

    Drawing Tilesgit add .!

--- TileFiller.java
+++ TileFiller.java
@@ -17,17 +17,15 @@ public class TileFiller implements LineFiller {
     // TODO: ... everything
     // TODO: This does draw something to the screen, but it sure is not a tile :D
     void fillLine(short[] line, int y){
-        if(y > 16){
-            id++;
-            if(id>17)id=0;
-            return;
-        }
+        if(y>16)return;
         int color = 0;
-        int color2 = 0;
+        id=0;
         for(int i = 0; i < 220; i++){
-            if(i>16)continue;
+            if(i%16==0){
+                id++;
+            };
             __inline_cpp__("
-                color = ((uint8_t (*)[256])tileSet)[0x11][i+y*10];
+                color = ((uint8_t (*)[256])tileSet)[id][(i%16)+y*16];
             ");
             color = palette[color];
             line[i] = color;
--- scripts/TilemapConverter.js
+++ scripts/TilemapConverter.js
@@ -24,6 +24,7 @@ let palette;
 promises.push(new Promise((ok, nok)=>{
     APP.getPalette(pal=>{
         palette = pal;
+        APP.log(palette.length);
         if(pal) ok();
         else nok();
     });
