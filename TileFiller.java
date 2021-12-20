9ec9b929d35cd262b40d439c999997e1b773823c
torbuntu <sorensentor@tuta.io>
2021-12-20 08:56:29 -0600

    Cleanup

--- TileFiller.java
+++ TileFiller.java
@@ -7,9 +7,12 @@ public class TileFiller implements LineFiller {
     ushort[] palette;
     int width;
     int height;
+    int color = 0;
+    
     TileFiller(ushort[] palette){
         this.palette = palette;
     }
+    
     void setMap(pointer map, pointer tileSet){
         this.tileSet = tileSet;
         
@@ -20,29 +23,17 @@ public class TileFiller implements LineFiller {
         ");
     }
     
-    int color = 0;
-    ubyte id = 0;
-    // TODO: Read the map to get the width/height.
-    // TODO: Get the correct tile based on the current y and x position
-    // TODO: This does draw something to the screen, but it sure is not a tile :D
-    // ((uint8_t*)map)[(x%16)+(y%16)*width] // is this formula accurate?
     void fillLine(short[] line, int y){
        // if(y > 16)return;
         for(int x = 0; x < 220; x++){
             __inline_cpp__("
-                
-                //color = ((uint8_t (*)[256])tileSet)
-                //[((uint8_t*)tileMap)[(x%16)+(y%16)*width]] // tile from map x,y
-                //[(x%16)+(y%16)*16]; // tile color
-                
                 color = ((uint8_t*)tileSet)
-                [(((uint8_t*)tileMap)[(x/16)+(y/16)*width])
+                [(((uint8_t*)tileMap)[(x/16)+(y/16)*width]) // map tile index
                 *256+
-                ((x%16)+(y%16)*16)];
+                ((x%16)+(y%16)*16)]; // tile color index
             ");
             color = palette[color];
             line[x] = color;
-            //System.out.println(id);
         }
     }
 }
