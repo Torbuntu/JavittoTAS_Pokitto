c3a442c863646e74fa9a148628ba1a7bbec6eafd
torbuntu <sorensentor@tuta.io>
2022-07-29 20:17:42 -0500

    grid based sliding movement

--- Main.java
+++ Main.java
@@ -21,6 +21,8 @@ class Main extends State {
     Bot bot;
     BotHead botHead;
     
+    ubyte movement = 0, direction = 0;
+    
     public static void main(String[] args){
         Game.run( TIC80.font(), new Main() );
     }
@@ -28,11 +30,11 @@ class Main extends State {
     void init(){
         screen = new TASMode(Miloslav.palette(), TIC80.font());
         bot = new Bot();
-        bot.setPosition(162,30);
+        bot.setPosition(112,80);
         bot.idle();
         
         botHead = new BotHead();
-        botHead.setPosition(162,22);
+        botHead.setPosition(112,72);
         
         x = 0;
         y = 0;
@@ -51,50 +53,67 @@ class Main extends State {
         c++;
         if(c==100){
             c = 0;
-            System.out.println(screen.fps());
+            System.out.println((int)screen.fps());
+            System.out.println("X: " + x);
         }
         screen.clear(0);
         
         if( Button.A.justPressed() ) {
-            Game.changeState( new Main() );
         }
             
         if( Button.B.justPressed() ){
-            mapSwitch = !mapSwitch;
-            if(mapSwitch){
-                screen.setMap(TileMaps.getFarmMap(), TileMaps.getTiles());
-            }else{
-                screen.setMap(TileMaps.getFarmMap(), TileMaps.getTiles());
+        }
+        
+        if( Button.C.justPressed() ){
+        }
+        
+        if(movement > 0){
+            movement--;
+            switch(direction){
+                case 0: x--;break;
+                case 1: y++;break;
+                case 2: x++;break;
+                case 3: y--;break;
+            }
+        } else {
+            if(Button.Down.isPressed()){
+                // TODO down ladder / drop platform
+                // y-=16;
+                movement = 16;
+                direction = 3;
+            } else
+            if(Button.Up.isPressed()){
+                // TODO, jump/ladder
+                // y+=16;
+                movement = 16;
+                direction = 1;
+            } else 
+            if(Button.Right.isPressed()){
+                // dog.x = dog.x+2;
+                // bot.x+=2;
+                // botHead.x+=2;
+                // x+=16;
+                movement = 16;
+                direction = 2;
+                bot.setMirrored(true);
+                botHead.setMirrored(true);
+                bot.walkHori();
+            } else
+            if(Button.Left.isPressed()){
+                // bot.x-=2;
+                // botHead.x-=2;
+                // x-=16;
+                movement = 16;
+                direction = 0;
+                bot.setMirrored(false);
+                botHead.setMirrored(false);
+                bot.walkHori();
+            } else {
+                bot.idle();
             }
         }
         
-        if(Button.Down.justPressed()){
-            // TODO down ladder / drop platform
-            y-=16;
-        }
-        if(Button.Up.justPressed()){
-            // TODO, jump/ladder
-            y+=16;
-        }
-        if(Button.Right.justPressed()){
-            // dog.x = dog.x+2;
-            // bot.x+=2;
-            // botHead.x+=2;
-            x+=16;
-            bot.setMirrored(true);
-            botHead.setMirrored(true);
-            bot.walkHori();
-        }else
-        if(Button.Left.justPressed()){
-            // bot.x-=2;
-            // botHead.x-=2;
-            x-=16;
-            bot.setMirrored(false);
-            botHead.setMirrored(false);
-            bot.walkHori();
-        }else {
-            bot.idle();
-        }
+        
 
         
         botHead.y+=vy;
